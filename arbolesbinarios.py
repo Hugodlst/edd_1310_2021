bd4ecfb7d84fe62554e22064cda8858c08c3c7cb
Hugodlst <72094946+Hugodlst@users.noreply.github.com>
2021-01-26 13:07:04 -0600

    24Enero_1310
    24Enero_1310

--- arbolesbinarios.py
+++ arbolesbinarios.py
@@ -8,13 +8,13 @@ class BinarySearchTree:
     def __init__(self):
         self.__root=None
 
-    def __insert(self, value):
+    def insert(self, value):
         #regla 1
         if self.__root==None:
             self.__root=NodoArbol(value, None, None)
         #regla 2
         else:
-            self.__insert(self.__root, value)
+            self.insert(self.__root, value)
 
     def __insert_(self, nodo, value):
         if nodo.data==value:
@@ -32,36 +32,37 @@ class BinarySearchTree:
             else:
                 self.__insert_(NodoArbol.right, value)
 
-    def __recorrido_in(self, nodo):
-        if  nodo != None:
-            self.__recorrido_in(nodo.left)
-            print(nodo.data, end=" , ")
-            self.__recorrido_in(nodo.right)
+    def __recorrido_in( self , nodo ):
+        if nodo != None:
+            self.__recorrido_in( nodo.left )
+            print( nodo.data , end=" , ")
+            self.__recorrido_in( nodo.right )
 
-    def __recorrido_pos( self, nodo ):
+    def __recorrido_pos( self , nodo ):
         if nodo:
             self.__recorrido_pos(nodo.left)
             self.__recorrido_pos(nodo.right)
             print(nodo.data, end=", ")
 
-    def __recorrido_pre(self,nodo):
+    def __recorrido_pre( self , nodo ):
         if nodo:
-            print(nodo.data, end=" , ")
-            self.__recorrido_pre(nodo.left)
-            self.__recorrido_pre(nodo.right)
+            print(nodo.data, end=", ")
+            self.__recorrido_pre( nodo.left )
+            self.__recorrido_pre( nodo.right )
 
-    def transversal(self, format="inorden"):
-        if format=="inorden":
+     def transversal(self,format="inorden"):
+        if format == "inorden":
+            print("inorden")
             self.__recorrido_in(self.__root)
-        elif format=="preorden":
-            print("Recorrido en pre")
+        elif format == "preorden":
+            print("preorden")
             self.__recorrido_pre(self.__root)
-        elif format=="posorden":
-            print("Posorden")
+        elif format == "posorden":
+            print("posorden")
             self.__recorrido_pos(self.__root)
         else:
-            print("Error, ese formato no existe")
-        print("")
+            print("Error, formato incompatible")
+        print()
 
     def search (self, value):
         if self.__root==None:
@@ -84,6 +85,57 @@ class BinarySearchTree:
             return self.__search(nodo.right, value)
 
     def remove(self, value):
+        if self.__root=None:
+            print("Nada que eliminar")
+        else:
+            self.__remove(None, None, self.__root, value):
+
+    def __remove(self, padre_hizq, padre_hder, actual, value):
+        if actual==None:
+            print("Caso base")
+            return None
+        elif actual.data==value: #caso base
+            print("Encontrado", actual.data)
+
+            #caso 1: hoja:
+            if actual.left==None and actual.right==None:
+                if padre_hizq!=None:
+                    padre_hizq.left=None
+                else:
+                    padre_hder.right=None
+
+            #cso 2: con un hijo
+            if(actual.left!=None and actual.right==None)or(actual.left==None and actual.right==None):
+                print("Es un nodo con hijo", actual.data)
+                if actual.left!=None:
+                    actual.data=actual.left.data
+                    actual.left=None
+                else:
+                    actual.data=actual.right.data
+                    actual.right=None
+
+
+            #caso 3: con los dos hijos
+
+
+            #return actual
+
+        elif value < actal.data:
+            print("Busca a la izquierda")
+            self.__remove(actual, None, actual.left, value)
+        else:
+            print("Buscar a la Derecha")
+            self.__remove(None, actual, actual.right, value)
+
+
+
+
+
+
+
+
+"""
+
         encontrado = self.search(value)
         #Caso 1
         if encontrado.left==None and encontrado.right==None:
@@ -94,5 +146,5 @@ class BinarySearchTree:
              (encontrado.left == None and encontrado.right != None)
              print("A eliminar: ", encontrado.data)
 
-
+"""
             poner search
